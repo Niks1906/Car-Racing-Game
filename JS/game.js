edf5e06a86842bd0c24a0e2e95bedda1e80264ef
Niks1906 <66248757+Niks1906@users.noreply.github.com>
2020-12-22 18:50:08 +0530

    Add files via upload

--- JS/game.js
+++ JS/game.js
@@ -13,22 +13,28 @@ class Game {
             form.display();
         }
         car1 = createSprite(100, 300);
+        car1.addImage(car1Image);
         car2 = createSprite(300, 300);
+        car2.addImage(car2Image);
         car3 = createSprite(500, 300);
+        car3.addImage(car3Image);
         car4 = createSprite(700, 300);
+        car4.addImage(car4Image);
         cars = [car1, car2, car3, car4];
     }
 
     play() {
         form.hide();
+        image(ground, 0, 0, displayWidth, displayHeight*3);
+        image(track,  0, -displayHeight*4, displayWidth, displayHeight*5);
         Player.getallPlayers();
         if (allPlayers !== undefined) {
-            var x = 175;
+            var x = 200;
             var y;
             var index = 0;
             for (var p in allPlayers) {
                 index = index + 1;
-                x = x + 200;
+                x = x + 230;
                 y = displayHeight - allPlayers[p].distance;
                 cars[index - 1].x = x;
                 cars[index - 1].y = y;
--- sketch.js
+++ sketch.js
@@ -6,6 +6,17 @@ var game;
 var gameState = 0;
 var car1, car2, car3, car4, cars;
 var allPlayers;
+var car1Image, car2Image, car3Image, car4Image;
+var ground, track;
+
+function preload() {
+    car1Image = loadImage("Images/car1.png");
+    car2Image = loadImage("Images/car2.png");
+    car3Image = loadImage("Images/car3.png");
+    car4Image = loadImage("Images/car4.png");
+    ground = loadImage("Images/ground.png");
+    track = loadImage("Images/track.jpg");
+}
 
 function setup() {
     createCanvas(windowWidth - 15, windowHeight - 15);
